8925e91240d990835804e39c8360675c8e41aae5
gavnish kumar <107535592+Gavnishkumar@users.noreply.github.com>
2024-02-16 01:07:03 +0530

    deploye backend

---
+++ Route/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { registerUser,authUser,allUsers } = require('../controller/userControllers');
+const protect = require('../middleware/authmiddleware');
+const router= express.Router();
+router.route('/').post(registerUser).get(protect,allUsers);
+router.route('/login').post(authUser);
+module.exports=router;
