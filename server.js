4d8f323876bd502286793bc7d2fd2184ec461fc1
gavnish kumar <107535592+Gavnishkumar@users.noreply.github.com>
2024-02-17 07:39:44 +0530

    changes

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const server=app.listen(PORT,()=>{
 const io=require('socket.io')(server,{
     pingTimeout: 60000,
     cors:{
-        origin:"https://g-chat-client.onrender.com/",
+        origin:"*",
     },
 });
 io.on("connection",(socket)=>{
