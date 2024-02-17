2ccab2b25642f939e34f5ffa9d5fedf8a80fa5f3
gavnish kumar <107535592+Gavnishkumar@users.noreply.github.com>
2024-02-17 11:28:30 +0530

    remove console

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const io=require('socket.io')(server,{
     },
 });
 io.on("connection",(socket)=>{
-    console.log("socket.io connected successfully");
+    // console.log("socket.io connected successfully");
     socket.on('setup',(userData)=>{
         socket.join(userData.data._id);
        
@@ -58,7 +58,7 @@ io.on("connection",(socket)=>{
     })
     socket.on("join chat",(room)=>{
         socket.join(room)
-        console.log("user joined room "+ room);
+        // console.log("user joined room "+ room);
     })
     socket.on('typing',(room)=>socket.in(room).emit("typing"));
     socket.on('stop typing',(room)=>socket.in(room).emit("stop typing"));
